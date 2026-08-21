4bda082760cbc9b92c8d4c57fcca7a2d646a64c6
Ghislain Leblanc <ghisleb@me.com>
2026-08-21 16:17:51 -0400

    Empty project

---
+++ Claude Switch/Claude Switch/Claude_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_SwitchApp.swift
+//  Claude Switch
+//
+//  Created by Ghislain Leblanc on 2026-08-21.
+//
+
+import SwiftUI
+
+@main
+struct Claude_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Claude Switch/Claude Switch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Claude Switch
+//
+//  Created by Ghislain Leblanc on 2026-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
